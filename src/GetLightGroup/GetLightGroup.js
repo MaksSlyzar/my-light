57a2b4251678df94835120f582c4316faff04830
MaksSlyzar <maks.slyzar@gmail.com>
2022-12-02 20:16:28 +0200

    add update-button

--- src/GetLightGroup/GetLightGroup.js
+++ src/GetLightGroup/GetLightGroup.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { getDayTime } from "../generate";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./GetLightGroup.css";
+import { Button } from "react-bootstrap";
+
 
 function vtime (time) {
     let hours = time[0], min = time[1];
@@ -21,16 +24,24 @@ function getDayName (day) {
 
 function GetLightGroup (props) {
     const group = props.group;
-    const day = new Date(Date.now()).getDay() - 1;
-    const hours = new Date(Date.now()).getHours();
-    const minutes = new Date(Date.now()).getMinutes();
-
-
-    const data = getDayTime(group, day, hours, minutes);
-    console.log(data);
+    const [ day, setDay ] = useState(new Date(Date.now()).getDay() - 1);
+    const [ hours, setHours ] = useState(new Date(Date.now()).getHours());
+    const [ minutes, setMinutes ] = useState(new Date(Date.now()).getMinutes());
+    const [ data, setData ] = useState(getDayTime(group, day, hours, minutes));
 
     const activities = ["відключення електроенергії", "електроенергія є", "можливе відключення"];
     const foractivities = ["відключення електроенергії", "електроенергія буде", "можливе відключення"];
+
+    const update = () => {
+        setDay(new Date(Date.now()).getDay() - 1);
+        setHours(new Date(Date.now()).getHours());
+        setMinutes(new Date(Date.now()).getMinutes());
+    
+    
+        setData(getDayTime(group, day, hours, minutes));
+
+        console.log("update")
+    }
 
     return (
         <div className="GetLightGroup">
@@ -39,7 +50,10 @@ function GetLightGroup (props) {
                 <div>{vtime([hours, minutes])}</div>
             </div>
 
+            
+
             <div className="atNow">
+            
                 <h5 className="">На даний момент 
                 <span className="activity"> {activities[data.now.activity]}</span></h5>
             </div>
@@ -49,6 +63,8 @@ function GetLightGroup (props) {
                 <span> {foractivities[data.next.activity]} до {vtime(data.next.to)}</span>
                 </h6>
             </div>
+
+            <Button onClick={() => update()} variant="primary">Оновити</Button>
         </div>
     )
 }
